fb146abadf82a1e6dc138dc798402c611d66ccc5
Lordemax <lordemax2012@gmail.com>
2019-07-29 04:04:59 +0100

    Create flash.py

---
+++ flash.py
@@ -0,0 +1,17 @@
+import telebot
+
+
+#TOKEN = "8:ZhumHmxps"
+
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
